df5e2bcb0bfc48ec9acd7bda85484176eece5fba
keshava <keshava.katakam@peopletech.com>
2025-04-08 23:49:33 +0530

    ui is done

--- src/main/header/header.tsx
+++ src/main/header/header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const Header = () => {
   return (
-    <Box sx={{ flexGrow: 1, height: "7%" }}>
+    <Box sx={{ flexGrow: 1, height: "7%", position: "sticky" }}>
       <AppBar
         position="static"
         color="transparent"
